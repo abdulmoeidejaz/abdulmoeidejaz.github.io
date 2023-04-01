e7a1af3393bf7a355ecb6bc5baf43ddf5d4f4bc2
Moeid Ejaz <abdulmoeid173@gmail.com>
2023-04-02 00:41:50 +0500

    changes

--- script.js
+++ script.js
@@ -5,9 +5,47 @@ const skyEl = document.getElementById("sky-el")
 const tempEl = document.getElementById("temp-el")
 const minTempEl = document.getElementById("min-temp-el")
 const maxTempEl = document.getElementById("max-temp-el")
+const timeEl = document.getElementById("time")
 
 const apiKey = "eeccf137789c6df5effe34534a5b2e8c"
 const url = "https://api.openweathermap.org/data/2.5/weather?q="
+
+
+
+
+// setInterval(() => {
+//     getTime()
+// }, 1000);
+
+const getTimeZone = async (country) => {
+    const response = await fetch(`https://api.timezonedb.com/v2.1/list-time-zone?key=IYV1N8O6KN5E&format=json&country=${country}`);
+    const data = await response.json();
+    // console.log(data)
+    console.log(data.zones[0].zoneName)
+    zoneName = data.zones[0].zoneName
+
+    setInterval(() => {
+        getTime(zoneName)
+    }, 1000);
+}
+const getTime = (timeZoneInfo)=>{
+    // const timeZoneName = timeZoneInfo
+    const date = new Date();
+    // const options = {
+    //     hour: "numeric",
+    //     minute: "numeric",
+    //     second: "numeric",
+    //     hour12: true,
+    //     timeZone: timeZoneInfo
+    //   };
+    const dateString = date.toLocaleDateString('en-US', { timeZone: timeZoneInfo });
+    const timeString = date.toLocaleTimeString('en-US', { timeZone: timeZoneInfo });
+    timeEl.innerHTML = "Time: " + timeString;
+
+    console.log("info: " + timeZoneInfo)
+    // return timeZoneInfo;
+}
+
 
 
 const getWeather = async (value) => {
@@ -15,13 +53,15 @@ const getWeather = async (value) => {
     let response = await fetch(`${url}${value}&appid=${apiKey}&units=metric`)
     let data = await response.json()
 
-    // console.log(data.weather[0].description)
+    // console.log(data)
+    // console.log("Country code: " + data.sys.country)
     cityTitle.innerText = value
 
     skyEl.innerText = `${data.weather[0].main}`
     tempEl.innerText = `Temp: ${data.main.temp}°C`
     minTempEl.innerText = `Min Temp: ${data.main.temp_min}°C`
     maxTempEl.innerText = `MaxTemp: ${data.main.temp_max} °C`
+    getTimeZone(data.sys.country)
 }
 
 
@@ -33,6 +73,7 @@ const search = () => {
     }
     getWeather(value)
     getBackground(value)
+    getTimeZone()
 }
 
 searchBtn.addEventListener("click", search)
@@ -52,6 +93,9 @@ const getlocation = async () => {
     // console.log(data)
     getWeather(data.city)
     getBackground(data.city)
+    // getTime(data.timezone)
+    // console.log("Time zone: " + data.timezone)
+    console.log(data.city)
 }
 
 getlocation()
@@ -60,21 +104,48 @@ const bodyEl = document.getElementById("body")
 
 const getBackground = async (city) => {
 
-    if(city === "Faisalabad"){
+
+    if (city === "Faisalabad") {
         let response = await fetch(`https://api.unsplash.com//search/photos?client_id=8Uzk2af1CnbW9ecswytWcW5-qxHL2AF2Czlup00frwA&query=${city}+clock+tower`)
         let data = await response.json()
         bodyEl.style.backgroundImage = `url('${data.results[0].urls.full}')`
-    } else{
+    } else {
         let response = await fetch(`https://api.unsplash.com//search/photos?client_id=8Uzk2af1CnbW9ecswytWcW5-qxHL2AF2Czlup00frwA&query=${city}`)
         let data = await response.json()
         bodyEl.style.backgroundImage = `url('${data.results[0].urls.full}')`
     }
-    // console.log(data.results[0].urls.full)
-   
-
-    // let backgroundUrl = data.results[0].urls.full
-
-    // bodyEl.style.backgroundImage = `url('')`;
 
 }
 
+    const timeEl01 = document.getElementById("time01")
+    const timeEl02 = document.getElementById("time02")
+    const timeEl03 = document.getElementById("time03")
+
+const updateTime = ()=>{
+    const pkTime = ()=>{
+        const date = new Date()
+        const dateString = date.toLocaleDateString('en-US', { timeZone: "Asia/Karachi" });
+        const timeString = date.toLocaleTimeString('en-US', { timeZone: "Asia/Karachi" });
+        timeEl01.innerHTML = "Time PK: " + timeString;
+    }
+    
+    const inTime = ()=>{
+        const date = new Date()
+        const dateString = date.toLocaleDateString('en-US', { timeZone: "Asia/Kolkata" });
+        const timeString = date.toLocaleTimeString('en-US', { timeZone: "Asia/Kolkata" });
+        timeEl02.innerHTML = "Time IN: " + timeString;
+    }
+    const dbTime = (country)=>{
+        const date = new Date()
+        const dateString = date.toLocaleDateString('en-US', { timeZone: country });
+        const timeString = date.toLocaleTimeString('en-US', { timeZone: country });
+        timeEl03.innerHTML = "Time DB: " + timeString;
+    }
+    pkTime()
+    inTime()
+    dbTime("Asia/Dubai")
+}
+
+// setInterval(() => {
+//     updateTime()
+// }, 1000);
