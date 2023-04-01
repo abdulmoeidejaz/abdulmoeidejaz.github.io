5bc1f4529f2e7d07f588b22e536a7a63016c26b0
Moeid Ejaz <abdulmoeid173@gmail.com>
2023-04-01 10:17:13 +0500

    changes

--- script.js
+++ script.js
@@ -63,12 +63,14 @@ const getBackground = async (city) => {
     if (city == 'Faisalabad' || 'faisalabad') {
         let response = await fetch(`https://api.unsplash.com//search/photos?client_id=8Uzk2af1CnbW9ecswytWcW5-qxHL2AF2Czlup00frwA&query=faisalabad+clock+tower`)
         let data = await response.json()
+        bodyEl.style.backgroundImage = `url('${data.results[0].urls.full}')`
     } else {
         let response = await fetch(`https://api.unsplash.com//search/photos?client_id=8Uzk2af1CnbW9ecswytWcW5-qxHL2AF2Czlup00frwA&query=${city}`)
         let data = await response.json()
+        bodyEl.style.backgroundImage = `url('${data.results[0].urls.full}')`
     }
     // console.log(data.results[0].urls.full)
-    bodyEl.style.backgroundImage = `url('${data.results[0].urls.full}')`
+   
 
     // let backgroundUrl = data.results[0].urls.full
 
